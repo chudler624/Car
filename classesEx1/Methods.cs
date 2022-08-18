244d2e2635236e5f65c58eb2b9e3487150642430
Casey Hudler <chudler624@gmail.com>
2022-08-18 12:51:35 -0400

    initial commit

---
+++ classesEx1/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesEx1
+{
+    internal class Methods
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ classesEx1/Program.cs
@@ -0,0 +1,9 @@
+﻿using classesEx1;
+
+
+Car myCar = new Car()
+{ Make = "honda", Model = "civic", Year = 2018, };
+
+Console.WriteLine($"Make {myCar.Make}");
+Console.WriteLine($"Model {myCar.Model}");
+Console.WriteLine($"Year {myCar.Year}");
